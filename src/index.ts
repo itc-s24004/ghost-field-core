2783b9d6073e63a9519b025cc16511bb1b86ef14
s24004 <s24004@std.it-college.ac.jp>
2026-02-19 16:49:31 +0900

    追加機能を呼び出せるように修正

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ export * from "./game.init.data.js";
 
 export * from "./card/index.js";
 export * from "./deck/index.js";
+export * from "./emitter/index.js";
 export * from "./game/index.js";
 export * from "./player/index.js";
 export * from "./table/index.js";
+export * from "./util/index.js";
 
 
 export const VERSION = "0.1.1";
